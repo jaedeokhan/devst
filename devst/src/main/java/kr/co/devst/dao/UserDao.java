7673f9fee321039ed0d9fab0aa4a673c7be4e372
jaedeokhan <dgd03023@cu.ac.kr>
2020-09-09 16:07:04 +0900

    modify repository

--- devst/src/main/java/kr/co/devst/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package kr.co.devst.dao;
-
-import kr.co.devst.model.UserVO;
-
-public interface UserDao {
-	public int doJoin(UserVO param);
-}
--- devst/src/main/java/kr/co/devst/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package kr.co.devst.service;
-
-import kr.co.devst.model.UserVO;
-
-public interface UserService {
-	public int doJoin(UserVO param);
-}
